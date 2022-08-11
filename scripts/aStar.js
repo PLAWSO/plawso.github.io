14d4ff0919e79817853afd8629fe578866b9f7ec
PLAWSO <64998692+PLAWSO@users.noreply.github.com>
2022-08-11 17:19:37 -0500

    added icons, pathfinder shield

--- scripts/aStar.js
+++ scripts/aStar.js
@@ -2,19 +2,9 @@ class AStar {
   constructor(deTiles, rows, cols, start, end) {
     this.rows = rows
     this.cols = cols
-    this.heap = []
+    this.deTiles = deTiles
 
-    this.tiles = []
-    for (let x = 0; x < rows; x++) {
-      let buffer = []
-      for (let y = 0; y < cols; y++) {
-        let newTile = new Tile(deTiles[y][x], y, x) // SWAP ???
-        buffer.push(newTile)
-      }
-      this.tiles.push(buffer)
-    }
-
-    
+    this.reset()
   }
   
   findEntities() {
@@ -32,60 +22,80 @@ class AStar {
     this.heap.push(this.start)
   }
 
+  reset() {
+    this.heap = []
+    this.tiles = []
+    for (let x = 0; x < this.rows; x++) {
+      let buffer = []
+      for (let y = 0; y < this.cols; y++) {
+        let newTile = new Tile(this.deTiles[y][x], y, x)
+        buffer.push(newTile)
+      }
+      this.tiles.push(buffer)
+    }
+
+    this.deTiles.forEach(tileRow => {
+      tileRow.forEach(tile => {
+        tile.classList.remove("neighbor", "visited", "closed", "path")
+      })
+    })
+
+    this.start = null
+    this.end = null
+  }
+
   algoStep() {
     if (!this.start)
       this.findEntities()
-/*
-    let z = 0
-    while (z < 3) {
-      z++
-*/
-      let currentTile = this.heap[0]
+
+    let currentTile
+    if (this.heap.length > 0) {
+      currentTile = this.heap[0]
       this.heap.splice(0, 1)
+    }
+    else
+      return [-1]
 
-
-      if (currentTile === this.end)
-        return this.pathTo(currentTile)
-      
-      currentTile.closed = true
-      currentTile.deTile.classList.add("closed")
-
-      let neighbors = this.neighbors(currentTile)
-
-      neighbors.forEach(neighbor => {
-        if (!neighbor.closed) {
-
-          let gScore = currentTile.g
-          if (currentTile.x != neighbor.x && currentTile.y != neighbor.y)
-            gScore += 14
-          else
-            gScore += 10
-
-          let visited = neighbor.visited
-
-          if (!visited || gScore < neighbor.g) {
-            neighbor.visited = true
-            neighbor.parent = currentTile
-            neighbor.g = gScore
-            neighbor.h = this.distanceTo(neighbor, this.end)
-            neighbor.f = neighbor.g + neighbor.h
-            neighbor.deTile.classList.add("visited")
-            neighbor.deTile.innerHTML = "g:"+neighbor.g+"\nh:"+neighbor.h+"\nf:"+neighbor.f+"\nx:"+neighbor.x+"\ny:"+neighbor.y
-          }
-
-          if (!visited)
-            this.heap.push(neighbor)
-          this.heap.sort((a, b) => a.f - b.f)
-
-          let buf = []
-          this.heap.forEach(tile => {
-            buf.push(tile.deTile)
-          })
-
-        }
-      })
+    if (currentTile === this.end)
+      return this.pathTo(currentTile)
     
-    //}
+    currentTile.closed = true
+    currentTile.deTile.classList.add("closed")
+
+    let neighbors = this.neighbors(currentTile)
+
+    neighbors.forEach(neighbor => {
+      if (!neighbor.closed) {
+
+        let gScore = currentTile.g
+        if (currentTile.x != neighbor.x && currentTile.y != neighbor.y)
+          gScore += 14
+        else
+          gScore += 10
+
+        let visited = neighbor.visited
+
+        if (!visited || gScore < neighbor.g) {
+          neighbor.visited = true
+          neighbor.parent = currentTile
+          neighbor.g = gScore
+          neighbor.h = this.distanceTo(neighbor, this.end)
+          neighbor.f = neighbor.g + neighbor.h
+          neighbor.deTile.classList.add("visited")
+        }
+
+        if (!visited)
+          this.heap.push(neighbor)
+        this.heap.sort((a, b) => a.f - b.f)
+
+        let buf = []
+        this.heap.forEach(tile => {
+          buf.push(tile.deTile)
+        })
+
+      }
+    })
+  
     return []
   }
 
